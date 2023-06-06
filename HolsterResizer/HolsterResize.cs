491d4bace59a8dc42e1d5edae79f301b0968b33e
Toedtmanns <erik.toedtmann@gmail.com>
2023-06-06 17:34:58 +0200

    Add project files.

---
+++ HolsterResizer/HolsterResize.cs
@@ -0,0 +1,132 @@
+﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using MelonLoader;
+using SLZ.Bonelab;
+using SLZ.Interaction;
+using SLZ.Player;
+using SLZ.Rig;
+using SLZ.VRMK;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+namespace HolsterResizer
+{
+    public class HolsterResize : MelonMod
+    {
+        public static MelonLogger.Instance Logger => Melon<HolsterResize>.Logger;
+        public static BonelabGameControl GameControl { get; private set; }
+        public static RigManager PlayerRig => GameControl.PlayerRigManager;
+
+        private static float _relativeSize;
+        public static float RelativeSize
+        {
+            get => _relativeSize;
+            set => _relativeSize = value / 1.8f;
+        }
+
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Loaded mod!");
+
+            GameControl = GameObject.FindObjectOfType(Il2CppType.Of<BonelabGameControl>()) as BonelabGameControl;
+
+            //PlayerRig.onAvatarSwapped += new Action(AvatarSwapCB);
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            GameControl = GameObject.FindObjectOfType(Il2CppType.Of<BonelabGameControl>()) as BonelabGameControl;
+        }
+
+        public static void AvatarSwapCB()
+        {
+            Logger.Msg($"Loaded avatar {PlayerRig.avatarID} with height {PlayerRig.avatar.height}");
+        }
+    }
+
+    public static class DebugStuff<TMod> where TMod : MelonBase
+    {
+        public static void PrintClassFields<TClass>(TClass c)
+        {
+            MemberInfo[] members = typeof(TClass).GetMembers();
+
+            Melon<TMod>.Logger.Msg(ConsoleColor.Magenta, $"Memberinfo of class {typeof(TClass).FullName}");
+            foreach (MemberInfo m in members)
+            {
+                if (m.MemberType == MemberTypes.Property)
+                {
+                    PropertyInfo p = m as PropertyInfo;
+                    Melon<TMod>.Logger.Msg($"{m.Name}: {p.GetValue(c)}");
+                }
+                else if (m.MemberType == MemberTypes.Field)
+                {
+                    FieldInfo f = m as FieldInfo;
+                    Melon<TMod>.Logger.Msg($"{m.Name}: {f.GetValue(c)}");
+                }
+            }
+        }
+
+        public static void StackTrace()
+        {
+            StackTrace sT = new StackTrace();
+            StackFrame[] stackFrames = sT.GetFrames();
+
+            foreach (StackFrame frame in stackFrames)
+            {
+                Melon<TMod>.Logger.Msg(frame.GetMethod().Name);
+            }
+        }
+    }
+
+    /// <summary>
+    /// This patch resizes all inventory body slots on avatar change. It also resizes the Items that are holstered
+    /// </summary>
+    [HarmonyPatch(typeof(RigManager), nameof(RigManager.SwitchAvatar))]
+    public static class SwapAvatarPatch
+    {
+        public static void Postfix(RigManager __instance, Avatar newAvatar)
+        {
+            //newAvatar.RefreshBodyMeasurements();
+            HolsterResize.RelativeSize = __instance.avatar.height;
+            float relSize = HolsterResize.RelativeSize;
+            //float relSize = __instance.avatar.height;
+
+            HolsterResize.Logger.Msg($"Loading avatar {__instance.avatar.name} with height abs: {__instance.avatar.height} rel: {relSize}!");
+
+            foreach (var v in __instance.inventory.bodySlots)
+            {
+                v.GetComponent<Transform>().localScale = new Vector3(relSize, relSize, relSize);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(InventorySlotReceiver), nameof(InventorySlotReceiver.OnHandDrop))]
+    public static class OnHandDropPatch
+    {
+        public static void Postfix(InventorySlotReceiver __instance, IGrippable host)
+        {
+            GameObject go = __instance._slottedWeapon.interactableHost.gameObject;
+            float relSize = HolsterResize.RelativeSize;
+            go.GetComponent<Transform>().localScale = new Vector3(relSize, relSize, relSize);
+        }
+    }
+
+    [HarmonyPatch(typeof(InventorySlotReceiver), nameof(InventorySlotReceiver.OnHandGrab))]
+    public static class OnHandGrabPatch
+    {
+        public static void Prefix(InventorySlotReceiver __instance, Hand hand)
+        {
+            GameObject go = __instance._slottedWeapon.interactableHost.gameObject;
+            float relSize = 1 / HolsterResize.RelativeSize;
+            go.GetComponent<Transform>().localScale = new Vector3(relSize, relSize, relSize);
+        }
+    }
+}
---
+++ HolsterResizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HolsterResizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HolsterResizer")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2ddb519-b45b-4fdf-9ec3-f1eb2cd4fd61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// MelonLoader mod stuff
+[assembly: MelonInfo(typeof(HolsterResizer.HolsterResize), "HolsterResizer", "0.1.0", "Toedtmanns")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+
+#if DEBUG
+[assembly: MelonColor(System.ConsoleColor.Red)]
+#else
+[assembly: MelonColor(System.ConsoleColor.Cyan)]
+#endif
